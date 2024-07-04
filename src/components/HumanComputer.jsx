8914c6a21c182bfa24df73f8d5abbc84b8749f2a
GersonEC <gersonenriquez94@gmail.com>
2024-07-04 18:32:35 +0200

    add game information to the human vs computer match

--- src/components/HumanComputer.jsx
+++ src/components/HumanComputer.jsx
@@ -1,38 +1,65 @@
+import { useEffect, useState } from "react";
 import { Play_moves } from "../utils/constants"
 
+// Function to calculate the winner of the game
+// 1 is returned if the human wins, 2 if the computer wins, and 0 if it is a tie
 const calculateWinner = (humanMove, computerMove) => {
   if (humanMove === computerMove) {
-    return 'It is a tie!';
+    return 0;
   } else if (
     (humanMove === 'stone' && computerMove === 'scissors') ||
     (humanMove === 'paper' && computerMove === 'stone') ||
     (humanMove === 'scissors' && computerMove === 'paper')
   ) {
-    return 'You win!';
+    return 1;
   } else {
-    return 'Computer wins!';
+    return 2;
   }
 }
 
 export const HumanComputer =  () => {
+  const [humanScore, setHumanScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [humanMove, setHumanMove] = useState('');
+  const [computerMove, setComputerMove] = useState('');
+  const [partialWinner, setPartialWinner] = useState('');
+  const thereIsAWinner = humanScore === 3 || computerScore === 3;
 
   const play = (move) => () => {
-    console.log(move)
     const computerMove = Play_moves[(Math.floor(Math.random() * 3) + 1).toString()];
-    console.log(computerMove)
+    setHumanMove(move);
+    setComputerMove(computerMove);
     const winner = calculateWinner(move, computerMove);
-    console.log(winner)
+    setPartialWinner(winner);
+    if (winner === 1) {
+      setHumanScore(humanScore + 1);
+    } else if (winner === 2) {
+      setComputerScore(computerScore + 1);
+    }
+  }
+
+  const handleRestart = () => {
+    setHumanScore(0);
+    setComputerScore(0);
   }
 
   return (
     <div>
       <h1>Human vs Computer</h1>
-      <h2>You: 0</h2>
-      <h2>Computer: 0</h2>
+      {!thereIsAWinner && <p>First to 3 wins!</p>}
+      <h2>You: {humanScore}</h2>
+      <h2>Computer: {computerScore}</h2>
       <p>Choose your next move:</p>
-      <button onClick={play('stone')}>{Play_moves['1']}</button>
-      <button onClick={play('paper')}>{Play_moves['2']}</button>
-      <button onClick={play('scissors')}>{Play_moves['3']}</button>
+      <button onClick={play('stone')} disabled={thereIsAWinner}>{Play_moves['1']}</button>
+      <button onClick={play('paper')} disabled={thereIsAWinner}>{Play_moves['2']}</button>
+      <button onClick={play('scissors')} disabled={thereIsAWinner}>{Play_moves['3']}</button>
+      {humanMove && computerMove && <p>{humanMove}🧻 vs {computerMove}🪨</p>}
+      {partialWinner === 1 && !thereIsAWinner && <p>You win this round!</p>}
+      {partialWinner === 2 && !thereIsAWinner && <p>Computer wins this round!</p>}
+      {partialWinner === 0 && !thereIsAWinner && <p>It's a tie!</p>}
+      {humanScore === 3 && <p>You win!</p>}
+      {computerScore === 3 && <p>Computer wins!</p>}
+      {thereIsAWinner && <button onClick={handleRestart}>Restart</button>}
     </div>
   )
 }
