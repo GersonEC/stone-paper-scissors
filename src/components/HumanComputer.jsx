f67452b8df76ab431f16ce05d813650b19a585cc
GersonEC <gersonenriquez94@gmail.com>
2024-07-04 12:39:10 +0200

    implememt human vs computer component

--- src/components/HumanComputer.jsx
+++ src/components/HumanComputer.jsx
@@ -1,4 +1,38 @@
+import { Play_moves } from "../utils/constants"
+
+const calculateWinner = (humanMove, computerMove) => {
+  if (humanMove === computerMove) {
+    return 'It is a tie!';
+  } else if (
+    (humanMove === 'stone' && computerMove === 'scissors') ||
+    (humanMove === 'paper' && computerMove === 'stone') ||
+    (humanMove === 'scissors' && computerMove === 'paper')
+  ) {
+    return 'You win!';
+  } else {
+    return 'Computer wins!';
+  }
+}
 
 export const HumanComputer =  () => {
-  return <h1>Human Computer</h1>
+
+  const play = (move) => () => {
+    console.log(move)
+    const computerMove = Play_moves[(Math.floor(Math.random() * 3) + 1).toString()];
+    console.log(computerMove)
+    const winner = calculateWinner(move, computerMove);
+    console.log(winner)
+  }
+
+  return (
+    <div>
+      <h1>Human vs Computer</h1>
+      <h2>You: 0</h2>
+      <h2>Computer: 0</h2>
+      <p>Choose your next move:</p>
+      <button onClick={play('stone')}>{Play_moves['1']}</button>
+      <button onClick={play('paper')}>{Play_moves['2']}</button>
+      <button onClick={play('scissors')}>{Play_moves['3']}</button>
+    </div>
+  )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const Play_moves = {
+  1: 'stone',
+  2: 'paper',
+  3: 'scissors'
+}
